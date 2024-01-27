232d7643ebe2ec2cb8545a70e62f04e0b2a8e0fa
VinceToy <roxy_toy@icloud.com>
2024-01-27 10:37:35 -0800

    made the reverse intake state

---
+++ src/main/java/frc/robot/commands/ReverseIntakeCommand.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Intake;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ReverseIntakeCommand extends InstantCommand {
+  private Intake reverseIntakeInstance;
+
+  public ReverseIntakeCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    reverseIntakeInstance = Intake.getInstance();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (reverseIntakeInstance.getCurrentState() == Intake.State.Intaking) {
+      reverseIntakeInstance.startReverseIntake();
+    } else if (reverseIntakeInstance.getCurrentState() == Intake.State.Idle) {
+      reverseIntakeInstance.startReverseIntake();
+    } else if (reverseIntakeInstance.getCurrentState() == Intake.State.ReverseIntake) {
+      reverseIntakeInstance.startIdle();
+    }
+  }
+}

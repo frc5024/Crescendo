1317ae4c31b3df5794f369108bf6ba7ecf717aaf
Anthony <hoanthony74@gmail.com>
2024-02-02 17:05:05 -0500

    test motors gone

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.lib.util.BatteryTracker;
@@ -36,9 +35,6 @@ public class Robot extends LoggedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
-
-    private Talon talonTesting;
-    private Talon talonTesting2;
 
     /**
      * This function is run when the robot is first started up and should be used
@@ -86,12 +82,6 @@ public class Robot extends LoggedRobot {
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
-        // This indicates what channel the motor is connected to.
-        talonTesting = new Talon(0);
-        talonTesting2 = new Talon(1);
-        // This makes the motor move at 0.4 speed.
-        talonTesting.set(0.4);
-        talonTesting2.set(0.4);
     }
 
     /**

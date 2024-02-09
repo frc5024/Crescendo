015988d828aabc524fbb652f04ff5c21dc9686aa
Anthony <hoanthony74@gmail.com>
2024-02-09 15:55:02 -0500

    fixed things

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "d9ea43dcc25f3336987c76ec22d112905b2b4abe";
-  public static final String GIT_DATE = "2024-02-03 12:54:57 EST";
-  public static final String GIT_BRANCH = "Shooter";
-  public static final String BUILD_DATE = "2024-02-03 13:28:51 EST";
-  public static final long BUILD_UNIX_TIME = 1706984931524L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "ac153433239d00f49b024e014cfdabb0aa220c28";
+  public static final String GIT_DATE = "2024-02-08 16:15:33 EST";
+  public static final String GIT_BRANCH = "kickerprototype";
+  public static final String BUILD_DATE = "2024-02-08 16:20:33 EST";
+  public static final long BUILD_UNIX_TIME = 1707427233280L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/commands/OuttakeCommand.java
+++ src/main/java/frc/robot/commands/OuttakeCommand.java
@@ -4,24 +4,49 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Kicker;
+import frc.robot.subsystems.Shooter;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class OuttakeCommand extends InstantCommand {
+public class OuttakeCommand extends Command {
   private Intake intakeInstance;
+  private Kicker kickerInstance;
+  private Shooter shooterInstance;
 
+  /** Creates a new IntakeCommand. */
   public OuttakeCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
     intakeInstance = Intake.getInstance();
+    kickerInstance = Kicker.getInstance();
+    shooterInstance = Shooter.getInstance();
     addRequirements(intakeInstance);
+    addRequirements(kickerInstance);
+    addRequirements(shooterInstance);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     intakeInstance.startOuttake();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intakeInstance.startIdle();
+    kickerInstance.startIdle();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
   }
 }

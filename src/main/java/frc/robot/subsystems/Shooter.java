54a2b263a5502f5acd8dfe219d39518e0c771cfa
wilbur <unnamed.wphillips@gmail.com>
2024-02-16 16:04:29 -0500

    jammed state no PID

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,11 +6,9 @@ import com.revrobotics.RelativeEncoder;
 import com.team5024.lib.statemachines.StateMachine;
 import com.team5024.lib.statemachines.StateMetadata;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ShooterConstants;
@@ -28,8 +26,6 @@ public class Shooter extends SubsystemBase {
     private RelativeEncoder m_rightEncoder;
     private Kicker kickerInstance;
     private DigitalInput linebreak;
-    private PIDController leftPIDController;
-    private PIDController rightPIDController;
 
     public static final Shooter getInstance() {
         if (mInstance == null) {
@@ -63,11 +59,6 @@ public class Shooter extends SubsystemBase {
 
         m_leftEncoder = leftMotor.getEncoder();
         m_rightEncoder = rightMotor.getEncoder();
-        leftPIDController = new PIDController(0.2, 0.2, 0);
-        rightPIDController = new PIDController(0.2, 0.2, 0);
-
-        leftMotor.setSmartCurrentLimit(ShooterConstants.ShooterCurrentLimit);
-        rightMotor.setSmartCurrentLimit(ShooterConstants.ShooterCurrentLimit);
 
         warmingUp = new Timer();
         warmingUp.reset();
@@ -83,10 +74,6 @@ public class Shooter extends SubsystemBase {
         Tab.addDouble("warming timer", () -> warmingUp.get());
         Tab.addDouble("shootTimer", () -> shootTimer.get());
         Tab.addBoolean("Linebroken", () -> linebreak.get());
-        SmartDashboard.putNumber("Shooter kP", 0.000050);
-        SmartDashboard.putNumber("Shooter max output", 0);
-        SmartDashboard.putNumber("Shooter setpoint (RPM)", 0);
-        SmartDashboard.putNumber("Shooter accelerator RPM", 0);
 
         stateMachine = new StateMachine<>("Shooter");
         stateMachine.setDefaultState(State.Idle, this::handleIdleState);
@@ -95,13 +82,6 @@ public class Shooter extends SubsystemBase {
         stateMachine.addState(State.Jammed, this::handleJammedState);
 
     }
-
-    // public void setFlywheelToRPM(double rpm) {
-    // leftMotor.getPIDController().setReference(rpm,
-    // CANSparkBase.ControlType.kVelocity);
-    // rightMotor.getPIDController().setReference(rpm,
-    // CANSparkBase.ControlType.kVelocity);
-    // }
 
     private void handleIdleState(StateMetadata<State> metadata) {
 
@@ -117,39 +97,15 @@ public class Shooter extends SubsystemBase {
         if (metadata.isFirstRun()) {
             warmingUp.reset();
             warmingUp.start();
-            leftPIDController.reset();
-            rightPIDController.reset();
-            leftPIDController.setTolerance(Constants.ShooterConstants.tolerance, Constants.ShooterConstants.errorDerivative);
-            rightPIDController.setTolerance(Constants.ShooterConstants.tolerance, Constants.ShooterConstants.errorDerivative);
-
-            // leftMotor.set(0.9);
-            // rightMotor.set(0.9);
+            leftMotor.set(0.9);
+            rightMotor.set(0.9);
         }
 
-        if (Constants.ShooterConstants.speaker = true) {
-            leftMotor.set(leftPIDController.calculate(m_leftEncoder.getVelocity(),
-                    Constants.ShooterConstants.speakerSetpoint));
-            rightMotor.set(rightPIDController.calculate(m_rightEncoder.getVelocity(),
-                    Constants.ShooterConstants.speakerSetpoint));
-        } else {
-            leftMotor.set(
-                    leftPIDController.calculate(m_leftEncoder.getVelocity(), Constants.ShooterConstants.ampSetpoint));
-            rightMotor.set(
-                    rightPIDController.calculate(m_rightEncoder.getVelocity(), Constants.ShooterConstants.ampSetpoint));
-        }
-
-        if (leftPIDController.atSetpoint() && rightPIDController.atSetpoint()) {
+        if (m_leftEncoder.getVelocity() >= Constants.ShooterConstants.speakerSetpoint
+                && m_rightEncoder.getVelocity() >= Constants.ShooterConstants.speakerSetpoint) {
             warmingUp.stop();
             stateMachine.setState(State.Shoot);
         }
-
-        // if (m_leftEncoder.getVelocity() >=
-        // Constants.ShooterConstants.shootVelocitySpeaker
-        // && m_rightEncoder.getVelocity() >=
-        // Constants.ShooterConstants.shootVelocitySpeaker) {
-        // warmingUp.stop();
-        // stateMachine.setState(State.Shoot);
-        // }
     }
 
     private void handleShootState(StateMetadata<State> metadata) {
@@ -184,32 +140,6 @@ public class Shooter extends SubsystemBase {
     @Override
     public void periodic() {
         stateMachine.update();
-
-        // double kp = SmartDashboard.getNumber("Shooter kP", 0);
-        // double maxOutput = SmartDashboard.getNumber("Shooter max output", 0);
-        // double setpointRPM = SmartDashboard.getNumber("Shooter setpoint (RPM)", 0);
-
-        // SparkPIDController leftPIDController = leftMotor.getPIDController();
-        // SparkPIDController rightPIDController = rightMotor.getPIDController();
-
-        // leftPIDController.setP(kp);
-        // leftPIDController.setOutputRange(-maxOutput, maxOutput);
-        // leftPIDController.setReference(setpointRPM,
-        // CANSparkBase.ControlType.kVelocity);
-        // rightPIDController.setP(kp);
-        // rightPIDController.setOutputRange(-maxOutput, maxOutput);
-        // rightPIDController.setReference(setpointRPM,
-        // CANSparkBase.ControlType.kVelocity);
-
-        SmartDashboard.putNumber("Left flywheel velocity",
-                m_leftEncoder.getVelocity());
-        SmartDashboard.putNumber("Right flywheel velocity",
-                m_rightEncoder.getVelocity());
-
-        // SmartDashboard.putNumber("Left flywheel error", m_leftEncoder.getVelocity() -
-        // setpointRPM);
-        // SmartDashboard.putNumber("Right flywheel error", m_rightEncoder.getVelocity()
-        // - setpointRPM);
     }
 
     public boolean isLineBroken() {

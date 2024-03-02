fb2009e24d463cb96b9aae790beaa3003e5180a6
kenn028 <kennyu0210@icloud.com>
2024-03-02 14:32:22 -0500

    Working 4 Note auto

--- src/main/java/frc/robot/commands/AimAndShootCommand.java
+++ src/main/java/frc/robot/commands/AimAndShootCommand.java
@@ -1,15 +1,20 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ShooterConstants.ShooterSetpoint;
+import frc.robot.subsystems.Shooter;
 
 public class AimAndShootCommand extends SequentialCommandGroup {
+  private final Shooter s_Shooter = Shooter.getInstance();
 
   public AimAndShootCommand(double armPosition, ShooterSetpoint shooterSetpoint) {
     addCommands(
         new ArmCommand(armPosition, shooterSetpoint),
-        new WaitCommand(1.0),
+        new InstantCommand(() -> s_Shooter.setWarmUp()),
+        Commands.waitUntil(() -> Shooter.getInstance().warmedUp()),
+        // new WaitCommand(1.0),
         new ShooterCommand());
   }
 }

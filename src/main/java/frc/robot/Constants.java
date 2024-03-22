16b7ab62992b817944d3aec15701f1475d824fc4
jbakela <jbakela@uwo.ca>
2024-03-22 13:44:00 -0400

    Changed maxSpeed to 4.0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -114,7 +114,7 @@ public final class Constants {
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 4.5; // TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 4.0; // TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 9.0; // TODO: This must be tuned to specific robot
 
